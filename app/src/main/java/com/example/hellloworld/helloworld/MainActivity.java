755fd526fdde13035d3b2535e7d9abfb0f56388f
张雅茹 <zhangyr@feicuiedu.com>
2016-10-31 21:14:29 +0800

    第三次提交

--- app/src/main/java/com/example/hellloworld/helloworld/MainActivity.java
+++ app/src/main/java/com/example/hellloworld/helloworld/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.hellloworld.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+    }
 
+    private void init() {
+        tv = (TextView) findViewById(R.id.tv);
     }
 }
